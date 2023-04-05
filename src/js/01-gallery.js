d473f3555af2c81d8588434e7ba1e71a5a4fb9b9
KonradSadowski <darnokowsky@gmail.com>
2023-04-05 20:38:41 +0200

    zadanie 1 zrobione

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEL = document.querySelector('.gallery');
+// creating img's
+for (const image of galleryItems) {
+  const imgEl = `<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+  <img 
+  class="gallery__image"
+  src=${image.preview}
+  data-source=${image.original}
+  alt=${image.description}/>
+  
+  </a>
+  </div>`;
+
+  galleryEL.insertAdjacentHTML('afterbegin', imgEl);
+}
+
+const imgClickHandler = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" >`
+  );
+  instance.show();
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape' || event.keyCode === 27) {
+      instance.close();
+    }
+  });
+};
+
+galleryEL.addEventListener('click', imgClickHandler);

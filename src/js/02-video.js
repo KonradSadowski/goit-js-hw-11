d43582fe750354121f9f0dce3d258b6fa66b90d0
KonradSadowski <darnokowsky@gmail.com>
2023-04-10 15:28:26 +0200

    komentarze

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,11 @@
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
+// odczyt czasu
 const saveVideoTime = throttle(
   data => localStorage.setItem('videoplayer-current-time', data.seconds),
   1000
 );
 player.on('timeupdate', saveVideoTime);
+// zapisanie czasu w local storage
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));

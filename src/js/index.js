84c7c52ca6bbccd54373bc132e2fb7aff31b81ea
KonradSadowski <darnokowsky@gmail.com>
2023-04-28 17:03:22 +0200

    pierwsze kroki

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,35 @@
-console.log(`Działa`);
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+
+const formEl = document.querySelector('.search-form');
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const loadButtonEl = document.querySelector('.load-button');
+
+const API_URL = `https://pixabay.com/api/`;
+const API_KEY = '35858797-639b225fbbec7c1b27496629b';
+
+let picturesShown = 40;
+let currentPage = 1;
+
+async function getPhotos() {
+  const response = await axios.get(API_URL, {
+    params: {
+      key: API_KEY,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      q: inputEl.value,
+      per_page: picturesShown,
+      page: currentPage,
+    },
+  });
+  console.log(response);
+}
+
+buttonEl.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(getPhotos());
+  loadButtonEl.style.display = 'flex';
+});
